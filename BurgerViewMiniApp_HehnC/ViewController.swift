f081e4f381e495cf42bcfeb31269c050d9f5e80d
Colin Hehn <colin45h@yahoo.com>
2020-10-27 14:17:24 -0500

    Initial Commit

---
+++ BurgerViewMiniApp_HehnC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BurgerViewMiniApp_HehnC
+//
+//  Created by Tiger Coder on 10/27/20.
+//  Copyright © 2020 clc.hehn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
